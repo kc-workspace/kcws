b0688b26a0d6c2f2df2e8f68ccb52afea4ceb5b6
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-08-31 21:04:55 +0700

    perf(hello): use inferred instead

--- packages/hello/vite.config.ts
+++ packages/hello/vite.config.ts
@@ -1,18 +1,19 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
-import * as path from 'path';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import * as path from "path";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/packages/hello',
+  cacheDir: "../../node_modules/.vite/packages/hello",
 
   plugins: [
     nxViteTsPaths(),
     dts({
-      entryRoot: 'src',
-      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+      entryRoot: "src",
+      tsconfigPath: path.join(__dirname, "tsconfig.lib.json"),
     }),
   ],
 
@@ -24,7 +25,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../../dist/packages/hello',
+    outDir: "../../dist/packages/hello",
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
@@ -32,12 +33,12 @@ export default defineConfig({
     },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
-      name: 'hello',
-      fileName: 'index',
+      entry: "src/index.ts",
+      name: "hello",
+      fileName: "index",
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs'],
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
@@ -48,13 +49,13 @@ export default defineConfig({
   test: {
     watch: false,
     globals: true,
-    environment: 'node',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    environment: "node",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
 
-    reporters: ['default'],
+    reporters: ["default"],
     coverage: {
-      reportsDirectory: '../../coverage/packages/hello',
-      provider: 'v8',
+      reportsDirectory: "../../coverage/packages/hello",
+      provider: "v8",
     },
   },
 });
