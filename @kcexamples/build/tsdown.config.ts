cafe9a26df8ecf7fb24630e86805f8edf79b266c
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2025-12-08 23:15:39 +0700

    perf(kcexamples): add typedoc to build package
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- @kcexamples/build/tsdown.config.ts
+++ @kcexamples/build/tsdown.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig, type UserConfig } from "@kcconfigs/tsdown";
 
-const config: UserConfig = defineConfig();
+const config: UserConfig = defineConfig({
+	platform: "node",
+	outDir: "dist.tsdown",
+});
 export default config;
