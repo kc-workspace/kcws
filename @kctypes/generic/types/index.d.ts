a19ff7706f653cef011d70a0d2acdc95a907d60c
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2025-12-08 21:29:08 +0700

    perf(kctypes): add generic and update package_json
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

---
+++ @kctypes/generic/types/index.d.ts
@@ -0,0 +1,32 @@
+declare global {
+	type WithNull<T> = T | null;
+	type WithUndefined<T> = T | undefined;
+	type Null = undefined | null;
+
+	type Optional<T> = T | undefined | null;
+	type Nullable<T> = Optional<T>; // alias
+
+	/**
+	 * Same as Required, for only K keys.
+	 * Anything else, will be partial.
+	 *
+	 * @beta
+	 */
+	type RequiredK<T, K extends keyof T = keyof T> = Required<
+		Pick<T, Extract<keyof T, K>>
+	> &
+		Partial<Omit<T, K>>;
+
+	/**
+	 * Same as Partial, for only K keys.
+	 * Anything else, will be required.
+	 *
+	 * @beta
+	 */
+	type PartialK<T, K extends keyof T = keyof T> = Partial<
+		Pick<T, Extract<keyof T, K>>
+	> &
+		Required<Omit<T, K>>;
+}
+
+export {};
