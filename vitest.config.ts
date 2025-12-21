26d67f6c28aaac5cbcee27c00d5f8e4edecd6808
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2025-12-21 11:17:27 +0700

    chore: prepare coverage threshold but not set yet
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,13 @@ import { defineRoot } from "@kcconfigs/vitest";
 export default defineRoot({
 	test: {
 		projects: ["packages/**/vitest.config.ts", "!packages/**/.*.old/**"],
+		coverage: {
+			thresholds: {
+				branches: 0,
+				functions: 0,
+				lines: 0,
+				statements: 0,
+			},
+		},
 	},
 });
