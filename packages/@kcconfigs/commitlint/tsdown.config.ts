3890ecc9bf3f094f1e6ab710ee5e23d525a6372b
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-03-07 11:05:16 +0700

    feat(kcconfigs/commitlint): set inlineOnly to bundle shared config instead of externalize
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- packages/@kcconfigs/commitlint/tsdown.config.ts
+++ packages/@kcconfigs/commitlint/tsdown.config.ts
@@ -2,5 +2,8 @@ import { defineConfig, type UserConfig } from "@kcconfigs/tsdown";
 
 const config: UserConfig = defineConfig({
 	platform: "node",
+	// inlineOnly incorrect identify the unused import, so we allow all instead of use whitelist mode
+	// ["@commitlint/types", "@commitlint/config-conventional", "conventional-commits-parser"]
+	inlineOnly: false,
 });
 export default config;
