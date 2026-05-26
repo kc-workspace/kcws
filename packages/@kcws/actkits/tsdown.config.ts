cca2dbcf282fb3dce3e55720bc6c27ed41efa66a
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-05-27 00:17:32 +0700

    fix(kcws/actkits): fix missing input entry from tsdown

--- packages/@kcws/actkits/tsdown.config.ts
+++ packages/@kcws/actkits/tsdown.config.ts
@@ -2,5 +2,6 @@ import { defineConfig, type UserConfig } from "@kcconfigs/tsdown";
 
 const config: UserConfig = defineConfig({
 	platform: "node",
+	entry: ["src/index.ts", "src/input/index.ts"],
 });
 export default config;
