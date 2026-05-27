187c0f473b2131b79ba75a6e8d2029b027e67be8
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-05-27 20:43:09 +0700

    perf(kcconfigs/tsdown): upgrade tsdown from v0.21 to v0.22

--- packages/@kcconfigs/tsdown/tsdown.config.ts
+++ packages/@kcconfigs/tsdown/tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, type UserConfig } from "./src/index";
 const config: UserConfig = defineConfig({
 	platform: "node",
 	unused: {
-		ignore: ["publint", "@arethetypeswrong/core", "unplugin-unused"],
+		ignore: ["publint", "@arethetypeswrong/core", "unplugin-unused", "unrun"],
 	},
 });
 export default config;
