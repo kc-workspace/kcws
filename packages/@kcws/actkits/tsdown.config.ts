422ead53122b31e333259e852057c1ed2136c59a
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-06-02 16:41:44 +0700

    perf(kcws/actkits): remove cjs
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- packages/@kcws/actkits/tsdown.config.ts
+++ packages/@kcws/actkits/tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, type UserConfig } from "@kcconfigs/tsdown";
 
 const config: UserConfig = defineConfig({
 	platform: "node",
-	entry: ["src/*/index.ts"],
-	format: "esm",
+	entry: ["src/index.ts", "src/*/index.ts"],
+	format: ["esm"],
 });
 export default config;
