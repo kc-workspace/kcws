50e66a9f6aa5950b380aa940a746746b1d586cd9
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-07-12 12:14:48 +0700

    perf(kcconfigs/vitest): migrate tsdown to new plugin system
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- packages/@kcconfigs/vitest/tsdown.config.ts
+++ packages/@kcconfigs/vitest/tsdown.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from "@kcconfigs/tsdown";
+import { defineConfig, type TsdownConfig } from "@kcconfigs/tsdown";
+import entryPlugin from "@kcconfigs/tsdown/plugins/entry";
+import nodePlugin from "@kcconfigs/tsdown/plugins/node";
 
-export default defineConfig({
-	platform: "node",
-	entry: ["./src/index.ts", "./src/mockHelpers/index.ts"],
-});
+const config: TsdownConfig = defineConfig(
+	entryPlugin(["./src/index.ts", "./src/mockHelpers/index.ts"]),
+	nodePlugin(),
+);
+export default config;
