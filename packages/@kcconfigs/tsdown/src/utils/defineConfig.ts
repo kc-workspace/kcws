3f0845fa6d518d5a70786aa38168ec2c97baa5d0
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-08-01 23:48:28 +0700

    refactor(kcconfigs/tsdown): simplify defineConfig by removing DefineOption parameter

--- packages/@kcconfigs/tsdown/src/utils/defineConfig.ts
+++ packages/@kcconfigs/tsdown/src/utils/defineConfig.ts
@@ -1,26 +1,22 @@
-import {
-	defineConfig as _defineConfig,
-	type DefineOption,
-} from "@kcinternals/config-builder";
+import { defineConfig as _defineConfig } from "@kcinternals/config-builder";
 import { baseConfig } from "../constants";
 import attwNormalize from "../internals/attwNormalize";
 import dtsNormalize from "../internals/dtsNormalize";
 import formatNormalize from "../internals/formatNormalize";
 import publintNormalize from "../internals/publintNormalize";
-import type { TsdownConfig, TsdownPlugin } from "../models";
+import type { TsdownConfig, TsdownConfigPlugin } from "../models";
 
 const defineConfig = (
-	option?: DefineOption,
-	...plugins: TsdownPlugin<string>[]
+	...plugins: TsdownConfigPlugin<string>[]
 ): TsdownConfig => {
-	const _plugins = [
+	return _defineConfig(
+		baseConfig,
 		...plugins,
 		attwNormalize(),
 		dtsNormalize(),
 		formatNormalize(),
 		publintNormalize(),
-	];
-	return _defineConfig(baseConfig, _plugins, option);
+	);
 };
 
 export default defineConfig;
