dd891b1943a12c7942c42391ce7adbd15cff2613
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-08-01 23:44:38 +0700

    perf(kcconfigs/commitlint): migrate tsdown config to new apis
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- packages/@kcconfigs/commitlint/tsdown.config.ts
+++ packages/@kcconfigs/commitlint/tsdown.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig, type TsdownConfig } from "@kcconfigs/tsdown";
-import depsPlugin from "@kcconfigs/tsdown/plugins/deps";
-import nodePlugin from "@kcconfigs/tsdown/plugins/node";
+import { depsPlugin, nodePlugin } from "@kcconfigs/tsdown/plugins";
 
 const config: TsdownConfig = defineConfig(
-	{},
 	nodePlugin(),
 	// inlineOnly incorrect identify the unused import, so we allow all instead of use whitelist mode
 	// ["@commitlint/types", "@commitlint/config-conventional", "conventional-commits-parser"]
