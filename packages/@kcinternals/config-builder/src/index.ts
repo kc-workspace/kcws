fbe2b58820633ab076e8568542e37c0fb59a98d1
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-08-02 17:33:00 +0700

    perf(kcinternals/config-builder): export withEnabled for WithEnabled helpers
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- packages/@kcinternals/config-builder/src/index.ts
+++ packages/@kcinternals/config-builder/src/index.ts
@@ -3,4 +3,5 @@ export type * from "./models";
 export { default as defineConfig } from "./utils/defineConfig";
 export { default as definePlugin } from "./utils/definePlugin";
 
+export { withEnabled } from "./utils/enabled";
 export { debug, verbose } from "./utils/logger";
