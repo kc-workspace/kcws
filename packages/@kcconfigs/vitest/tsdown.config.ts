7adb9b952080cab3071227b3184a4ea903a65e67
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-08-02 18:24:58 +0700

    feat(kcconfigs/vitest): export plugin entrypoints

--- packages/@kcconfigs/vitest/tsdown.config.ts
+++ packages/@kcconfigs/vitest/tsdown.config.ts
@@ -2,7 +2,11 @@ import { defineConfig, type TsdownConfig } from "@kcconfigs/tsdown";
 import { entryPlugin, nodePlugin } from "@kcconfigs/tsdown/plugins";
 
 const config: TsdownConfig = defineConfig(
-	entryPlugin(["./src/index.ts", "./src/mockHelpers/index.ts"]),
+	entryPlugin([
+		"./src/index.ts",
+		"./src/plugins/*.ts",
+		"./src/mockHelpers/index.ts",
+	]),
 	nodePlugin(),
 );
 export default config;
