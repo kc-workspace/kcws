c748c0cb1c2a4e24f24c6839b635345ef8c12396
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-08-20 22:36:12 +0700

    chore: revert debug in vitest

--- packages/@kcws/zconfig/vitest.config.ts
+++ packages/@kcws/zconfig/vitest.config.ts
@@ -1,14 +1,6 @@
 import { defineProjectConfig } from "@kcconfigs/vitest";
-import {
-	debugPlugin,
-	overridePlugin,
-	useMockPlugin,
-} from "@kcconfigs/vitest/plugins";
+import { useMockPlugin } from "@kcconfigs/vitest/plugins";
 
 export default defineProjectConfig(
-	overridePlugin({
-		printConsoleTrace: true,
-	}),
-	debugPlugin(),
 	useMockPlugin({ flags: { fs: true, fsPromises: true } }),
 );
